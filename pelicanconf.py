3adf4b554adf07f07f324b4b57043a63f3e33a2e
Jorge J. Riera <jorge.riera9@gmail.com>
2017-05-26 16:48:48 -0300

    agregando footer link FSF

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,11 +21,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
-#         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         )
-
+LINKS = (('Free Software Foundation', 'https://my.fsf.org/civicrm/profile/create?gid=496&reset=1'),)
+         
 # Social widget
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
